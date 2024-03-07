8c50ab3056f94b2837458579f8688ca0433f50af
Saji <Saji@A-5CG94703SB>
2024-03-07 09:39:00 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /*This is a comment is about introducing myself!*/
+        static void Main(string[] args)
+        {
+            string myName = "Jimmer Santos";
+            int myAge = 35;
+            double mySalary = 100;
+            char favoriteLetter = 'J';
+            bool isHappy = false;
+
+            Console.WriteLine("Jimmer Santos");
+            Console.Write("Hi ");
+            Console.Write("My Name is Jimmer");
+        }
+    }
+}
